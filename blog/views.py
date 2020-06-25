95eb0dceb72bfa9d153547027baa60b54f886d3b
lbb712 <lbreesetovey@gmail.com>
2020-06-25 14:38:00 +0100

    Added an authentication system that requires a user to be logged in in order to edit and create blog posts.

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from django.utils import timezone
 from .forms import PostForm
-import tkinter as tk
-from tkinter import messagebox
+from django.contrib.auth.decorators import login_required
 
 
 def post_list(request):
@@ -13,7 +12,8 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
-    
+
+@login_required    
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -26,7 +26,8 @@ def post_new(request):
         form = PostForm()
         
     return render(request, 'blog/post_edit.html', {'form': form})
-    
+
+@login_required    
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk =pk)
     if request.method == "POST":
@@ -39,18 +40,19 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
-    
+
+@login_required    
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('-created_date')
     return render(request, 'blog/post_draft_list.html', {'posts':posts})
-    
+
+@login_required    
 def post_publish (request,pk):
     post = get_object_or_404(Post, pk= pk)
     post.publish()
     return redirect('post_detail', pk=pk)
     
-
-    
+@login_required    
 def post_remove (request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
