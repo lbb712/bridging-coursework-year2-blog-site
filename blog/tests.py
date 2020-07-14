54dfaa1ce191f9568f2b81f74eaa2da9fb4a468c
lbb712 <lbreesetovey@gmail.com>
2020-07-14 12:47:51 +0100

    Updated the unit test so it checks the correct template is being used rather than testing constants.

--- blog/tests.py
+++ blog/tests.py
@@ -5,15 +5,7 @@ from django.http import HttpRequest
 
 
 class PostListTest(TestCase):
-    
-    def test_root_url_resolves_to_post_list_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, post_list)
         
     def test_base_returns_correct_html(self):
-        request = HttpRequest()
-        response = post_list(request)
-        html = response.content.decode('utf8')
-        #self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>My blog</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'blog/base.html')
