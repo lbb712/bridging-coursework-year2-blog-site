274f6124efa385d23c9d491261db0f0a1ef99433
lbb712 <lbreesetovey@gmail.com>
2020-07-14 11:38:44 +0100

    Added a couple of unit tests to test the post list view.

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.urls import resolve
+from blog.views import post_list
+from django.http import HttpRequest
 
-# Create your tests here.
+
+class PostListTest(TestCase):
+    
+    def test_root_url_resolves_to_post_list_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, post_list)
+        
+    def test_base_returns_correct_html(self):
+        request = HttpRequest()
+        response = post_list(request)
+        html = response.content.decode('utf8')
+        #self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>My blog</title>', html)
+        self.assertTrue(html.endswith('</html>'))
