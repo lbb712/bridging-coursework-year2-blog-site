500b443c0155fff41f9a33e4e86821408f84e024
lbb712 <lbreesetovey@gmail.com>
2020-08-20 11:17:35 +0100

    Added new functional tests.

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -12,20 +14,37 @@ class NewVisitorTest(unittest.TestCase):
         #Alice has heard about a new online CV app. She goes to check out its home page.
         self.browser.get('http://localhost:8000')
 
-        #She notices the page title and header mention CV editor
+        #She notices the page title and a URL mention CV editor
         self.assertIn('CV', self.browser.title)
-        self.fail('Finish the test!')
+        #link_text = self.browser.find_element_by_tag_name('li').text
+        #self.assertIn('CV', link_text)
+        
 
         #She follows the CV link in the top menu header
-
+        self.browser.get('http://localhost:8000/cvedit')
+        
         #She is invited to enter her name straight away
-
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            imputbox.get_attribute('placeholder'),
+            'Enter your name'
+        )
+        
         #She types "Alice" into a text box
+        inputbox.send_keys('Alice')
 
         #When she presses enter, the page updates and she sees her name in the form.
-
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')  
+        self.assertTrue(
+            any(row.text == 'Name: Alice' for row in rows)
+        )
+        
         #There is another text box inviting her to input her date of birth. She inputs "19/11/1995"
-
+        self.fail('Finish the test!')
         #The page updates again showing both items.
 
         #Alice wonders whether the site will remember her details. Then she sees that the site has generated a uniques URL for her -- there is some explanatory text to that effect.
