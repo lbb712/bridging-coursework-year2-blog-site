38e379179fb3c97adc3d14575d01ef3ae9785a17
lbb712 <lbreesetovey@gmail.com>
2020-08-20 16:08:29 +0100

    Added helper function to functional tests.

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
         
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+        
     def test_can_start_a_CV_and_retrieve_it_later(self):
         #Alice has heard about a new online CV app. She goes to check out its home page.
         self.browser.get('http://localhost:8000')
@@ -35,18 +40,21 @@ class NewVisitorTest(unittest.TestCase):
 
         #When she presses enter, the page updates and she sees her name in the form.
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(1) 
+        self.check_for_row_in_list_table('Name: Alice')
+        
+        #There is another text box inviting her to input her surname. She inputs Smith
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Smith')
+        inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')  
-        self.assertIn('Name: Alice', [row.text for row in rows])
-        
-        #There is another text box inviting her to input her date of birth. She inputs "19/11/1995"
-        self.fail('Finish the test!')
         #The page updates again showing both items.
-
+        self.check_for_row_in_list_table('Name: Alice')
+        self.check_for_row_in_list_table('Surname: Smith')
+   
         #Alice wonders whether the site will remember her details. Then she sees that the site has generated a uniques URL for her -- there is some explanatory text to that effect.
-
+        self.fail('Finish the test!')
         #She visits that URL -her details are still there.
 
         #Satisfied, she leaves the site to return at a later date.
